3b2ca629cea320e5e51a7da6edef707ed0e0e670
dvdxmn3 <eduardav.bs22@gmail.com>
2019-09-11 13:21:29 -0300

    aula 0409

--- src/java/modelos/Produto.java
+++ src/java/modelos/Produto.java
@@ -1,15 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package modelos;
 
 import java.util.ArrayList;
+
+/**
+ *
+ * @author gutol
+ */
 public class Produto {
     private String descricao;
     private float preco;
-    public static ArrayList<Produto> Lista = new ArrayList();
+    private boolean oferta;
+    private static ArrayList<Produto> lista = new ArrayList();
     
     public String getDescricao(){
-    return this.descricao;
+        return this.descricao;
     }
-    public void setDescricao(String descricao) {
+    
+    public void setDescricao(String descricao){
         this.descricao = descricao;
     }
 
@@ -20,18 +32,22 @@ public class Produto {
     public void setPreco(float preco) {
         this.preco = preco;
     }
+
+    public static ArrayList<Produto> getLista() {
+        return lista;
+    }
+
+    public static void setLista(ArrayList<Produto> lista) {
+        Produto.lista = lista;
+    }
+
+    public boolean isOferta() {
+        return oferta;
+    }
+
+    public void setOferta(boolean oferta) {
+        this.oferta = oferta;
+    }
+        
+    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
